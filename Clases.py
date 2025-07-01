8e024c28e8d499788784fcc345e8036196ad1ed5
Jhon Chavez <jhonf.6520@gmail.com>
2025-06-30 23:50:02 -0500

    Clases

--- Clases.py
+++ Clases.py
@@ -5,18 +5,22 @@ import time
 
 controlador = webdriver.Chrome()
 controlador.get("https://www.udemy.com/join/login-popup/?next=https%3A%2F%2Fwww.udemy.com%2Fmobile%2Fipad%2F&passwordredirect=True&skip_suggest=1")
+time.sleep(1)
 
-usuario = controlador.find_element(By.ID,'form-group--1')
-time.sleep(3)
-clave = controlador.find_element(By.ID,"form-group--3")
-time.sleep(3)
+usuario = controlador.find_element(By.CLASS_NAME,"-ud-compact-form-control")
+time.sleep(2)
+clave = controlador.find_element(By.CLASS_NAME,"password-form-group--password-input--yB71S")
+time.sleep(2)
 
 usuario.send_keys("jhonf.6520@gmail.com")
+time.sleep(2)
 clave.send_keys("Jf92092666940")
 time.sleep(2)
 
-boton = controlador.find_element(By.XPATH, "//button[contains(@class, 'helpers--auth-submit-button--W3Tqk')]")
+boton = controlador.find_element(By.CLASS_NAME,"ud-text-input")
 boton.click()
 time.sleep(3)
 
 controlador.quit()
+
+
--- Drivers/Nombre.py
+++ Drivers/Nombre.py
@@ -4,19 +4,20 @@ import time
 
 
 controlador = webdriver.Chrome()
-controlador.get("https://www.udemy.com/join/login-popup/?next=https%3A%2F%2Fwww.udemy.com%2Fmobile%2Fipad%2F&passwordredirect=True&skip_suggest=1")
 
-usuario = controlador.find_element(By.ID,'form-group--1')
-time.sleep(3)
-clave = controlador.find_element(By.ID,"form-group--3")
-time.sleep(3)
+controlador.get("https://www.udemy.com/join/login-popup/?next=https%3A%2F%2Fwww.udemy.com%2Fmobile%2Fipad%2F&passwordredirect=True&skip_suggest=1")
+time.sleep(2)
+
+usuario = controlador.find_element(By.NAME,"email")
+time.sleep(2)
+clave = controlador.find_element(By.NAME, "password")
+time.sleep(2)
 
 usuario.send_keys("jhonf.6520@gmail.com")
+time.sleep(2)
 clave.send_keys("Jf92092666940")
 time.sleep(2)
 
-boton = controlador.find_element(By.XPATH, "//button[contains(@class, 'helpers--auth-submit-button--W3Tqk')]")
+boton = controlador.find_element(By.XPATH,"//button[contains(@class, 'helpers--auth-submit-button--W3Tqk')]")
 boton.click()
-time.sleep(3)
 
-controlador.quit()
